1ab1210a4b6c68531c51a69d3a317cc6f9d0f750
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-22 11:47:40 +0200

    Replace placeholder text with "Random Person Generator" in app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <div><p>Hejsan</p></div>
+    <div>
+      <text class="font-bold flex justify-center text-white text-5xl py-10"
+        >Random Person Generator</text
+      >
+    </div>
   </div>
 </template>
