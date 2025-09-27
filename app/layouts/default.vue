6eb75f3871403efb765e9675c670054a4294188e
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-27 14:31:03 +0200

    Add default layout template for the application

---
+++ app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav class="flex flex-row p-4 justify-between">
+      <p class="text-white text-4xl">
+        <a href="/">Random Person Generator</a>
+      </p>
+      <p class="text-white">Testing</p>
+    </nav>
+  </div>
+</template>
