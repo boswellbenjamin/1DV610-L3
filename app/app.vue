ae3bec200e30a5873d3bc52e754c652b22e67da7
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-27 14:30:46 +0200

    Remove placeholder text and simplify app.vue structure

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,4 @@
 <template>
-  <div>
-    <div>
-      <text class="font-bold flex justify-center text-white text-5xl py-10"
-        >Random Person Generator</text
-      >
-    </div>
-  </div>
+  <NuxtLayout />
+  <NuxtPage />
 </template>
