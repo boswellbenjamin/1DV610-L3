aa061230975c5a977dabe77559d1f6b2412bd89f
Benjamin Boswell <bb222ny@student.lnu.se>
2025-09-27 14:40:38 +0200

    Add test function and button to index.vue for person data generation

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { Person } from "person-data-generator";
+
+function test() {
+  const person = Person.random();
+  console.log(`${person.getName()} ${person.getSurname()}`);
+}
+</script>
+
 <template>
   <p>testing</p>
+  <button @click="test" class="bg-blue-500 text-white px-4 py-2">
+    Styled button
+  </button>
 </template>
